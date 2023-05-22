795f914b8db425f24b12480ba38b0b643af5471e
raiztlin <billy91larsson@gmail.com>
2023-05-22 10:15:03 +0200

    Added TextField.

--- app/src/main/java/com/example/testingground/MainActivity.kt
+++ app/src/main/java/com/example/testingground/MainActivity.kt
@@ -17,13 +17,16 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -38,7 +41,10 @@ import com.example.testingground.ui.theme.TestingGroundTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import kotlinx.coroutines.delay
+import java.security.AllPermission
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,15 +57,17 @@ class MainActivity : ComponentActivity() {
                     .background(color = Color.Black)
             ) {
                 TimerComposable()
+
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TimerComposable(){
-    val time = 10
-    var counter by remember { mutableStateOf(time) }
+    var acceptInput by remember { mutableStateOf(true) }
+    var counter by remember { mutableStateOf("0") }
     var isRunning by remember { mutableStateOf(false) }
     var reset by remember { mutableStateOf(false) }
 
@@ -70,23 +78,41 @@ fun TimerComposable(){
 
         LaunchedEffect(key1 = isRunning, key2 = reset) {
             if (reset) {
-                counter = time
+                //counter = time // oklart hur den här logiken ska se ut
+                counter = "0"
                 reset = !reset
             } else if (isRunning) {
-                for (i in 1..counter) {
+                for (i in 1..counter.toInt()) {
                     delay(1000L)
-                    counter = counter - 1
+                    counter = (counter.toInt() - 1).toString()
                 }
             } else
                 counter = counter
         }
 
-        Text(
-            text = "${counter}",
-            color = Color.White,
-            fontSize = 200.sp,
-            fontWeight = FontWeight.Bold
-        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+
+        if (isRunning && !acceptInput) { // Appen krashar om jag har den här if sattsen.
+            Text(
+                text = "${counter}",
+                color = Color.White,
+                fontSize = 200.sp,
+                fontWeight = FontWeight.Bold
+            )
+        } else
+            TextField(
+                value = "${counter}",
+                label = {Text("Set timer")},
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.NumberPassword),
+                onValueChange = {
+                    counter = it
+                }
+            )
+            //SetTimerCount(onInput = { counter = it.toInt()})
+        }
 
         Row(
             Modifier.fillMaxWidth(),
@@ -96,6 +122,7 @@ fun TimerComposable(){
             Button(
                 onClick = {
                     isRunning = !isRunning
+                    acceptInput = !acceptInput
                 },
                 modifier = Modifier
                     .size(height = 50.dp, width = 100.dp)
@@ -108,27 +135,39 @@ fun TimerComposable(){
                 )
             }
 
-            if (isRunning || counter < time) {
-                Button(
-                    onClick = {
-                        reset = !reset
-                        if (isRunning){
-                            isRunning = !isRunning
-                        }
-                    },
-                    modifier = Modifier
-                        .size(height = 50.dp, width = 140.dp)
-                        .padding(start = 16.dp)
-                ) {
-                    Text(
-                        text = "Reset",
-                        color = Color.White,
-                        fontSize = 20.sp,
-                        fontWeight = FontWeight.Bold
-                    )
-                }
-            } else
-                true
+            //if (isRunning && !acceptInput) {
+                //Button(
+                    //onClick = {
+                        //reset = !reset
+                        //if (isRunning){
+                            //isRunning = !isRunning
+                        //}
+                    //},
+                    //modifier = Modifier
+                        //.size(height = 50.dp, width = 140.dp)
+                        //.padding(start = 16.dp)
+                //) {
+                    //Text(
+                        //text = "Reset",
+                        //color = Color.White,
+                        //fontSize = 20.sp,
+                        //fontWeight = FontWeight.Bold
+                    //)
+                //}
+            //}
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SetTimerCount(onInput: (String) -> Unit){
+    TextField(
+        value = "0",
+        label = {Text("Set timer")},
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.NumberPassword),
+        onValueChange = {
+            onInput(it)
+        }
+    )
 }
